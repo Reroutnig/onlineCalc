fce71f0599150b458667049cc09f95e1cf6f9a54
Nishat Quyoum <nishatq9006@gmail.com>
2023-10-01 20:28:31 -0700

    add ContWPrevAns function

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
-
 (function(){
-let screen = document.querySelector('.input');
-let numpad = document.querySelectorAll('.button');
-let clear = document.querySelector('.clear');
-let equal = document.querySelector('.equal');
-let answer=null;
-//stores value of button clicked
-function buttonClicked(value){
-    if(answer !== null) {
-        // If there is a previous answer, clear it and start a new calculation
-        screen.value = '';
-        answer = null;
-    }
-    screen.value += value;
-}
+    let screen = document.querySelector('.input');
+    let numpad = document.querySelectorAll('.button');
+    let clear = document.querySelector('.clear');
+    let equal = document.querySelector('.equal');
+    let answer = null;
 
-// displays numbers on screen when clicked
-    numpad.forEach(function(button){
-        button.addEventListener('click', function(e) {
-            let value=e.target.dataset.val;
-            buttonClicked(value);
-        })
-    });
-    function equalClickNext(){
-        if(screen.value=answer && equal.addEventListener('click')){
-            screen="";
+    // Function to handle numerical button click
+    function ContWithPrevAns(value){
+        if(answer !== null && screen.value === "") {
+            // If there is a previous answer and screen is empty, use the answer for the new calculation
+            screen.value = answer + value;
+        } else {
+            screen.value += value;
         }
     }
-//when equal button is clicked shows answer
-    equal.addEventListener('click', function(e){
-        //checks if there's any values on screen
-        if(screen.value === ''){ //if empty then dont show anything when equal sign is clicked
-            screen.value = "";
-        }else{
-             answer = eval(screen.value); 
-            screen.value = answer;
-        }
+
+    
+
+ //when equal button is clicked shows answer
+ equal.addEventListener('click', function(e){
+    //checks if there's any values on screen
+    if(screen.value === ''){ //if empty then dont show anything when equal sign is clicked
+        screen.value = "";
+    }else{
+         answer = eval(screen.value); 
+        screen.value = answer;
+    }
+})
+
+// displays numbers and operations on screen when clicked
+numpad.forEach(function(button){
+    button.addEventListener('click', function(e) {
+        let value=e.target.dataset.val;
+        ContWithPrevAns(value);
     })
+});
 
-    //to clear screen when 'c' is clicked
+   
+    // when 'c' is clicked the screen will clear
     clear.addEventListener('click', function(e){
         screen.value = "";
-    })
-   
+        answer = null;
+    });
 })();
+
