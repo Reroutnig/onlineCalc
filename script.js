f18590f8f9f4db5ca46b7088205b1ff129bde3b2
Nishat Quyoum <nishatq9006@gmail.com>
2023-10-03 16:26:45 -0700

    modified 'evalEquation' functiom

--- script.js
+++ script.js
@@ -5,8 +5,8 @@
     let equal = document.querySelector('.equal');
     let answer = null;
 
-    // Function to evaluate mathematical expression using switch cases
-    function evaluateExpression(expression) {
+    // evaluates math equations given by user
+    function evalEquation(expression) {
         let operators = expression.split(/[\d.]+/).filter(Boolean);
         let numbers = expression.split(/[\+\-\*\/]/).map(parseFloat);
         let result = numbers[0];
@@ -45,23 +45,23 @@
     }
 
     
-    function ContWithPrevAns(value){
+    function ContWPrevAns(value){
         if(answer !== null && screen.value === "") {
-            // If there is a previous answer and screen is empty, use the answer for the new calculation
+            // If there is a previous answer and the screen is empty, use the answer for the new calculation
             screen.value = answer + value;
         } else {
             screen.value += value;
         }
     }
 
-    // When equal button is clicked, evaluate the expression and display the answer
+    // When equal button is clicked, evaluate the equation and displays answer
     equal.addEventListener('click', function(e){
         // Checks if there's any values on screen
         if(screen.value === '') {
-            // If empty, do nothing
+            // does nothing if there nothing on screen
             screen.value = "";
         } else {
-            answer = evaluateExpression(screen.value);
+            answer = evalEquation(screen.value);
             screen.value = (answer === 'Error') ? 'Error' : parseFloat(answer.toFixed(2)); // Round the result to 2 decimal places
         }
     });
@@ -70,7 +70,7 @@
     numpad.forEach(function(button){
         button.addEventListener('click', function(e) {
             let value = e.target.dataset.val;
-            ContWithPrevAns(value);
+            ContWPrevAns(value);
         });
     });
 
@@ -80,4 +80,3 @@
         answer = null;
     });
 })();
-
